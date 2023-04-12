92f32615619a76ede8ab4a4ac8089126363b0eec
d-park7 <davidpark450@gmail.com>
2023-04-11 22:37:10 -0500

    updated

--- webscrapper.py
+++ webscrapper.py
@@ -1,159 +1,216 @@
-import requests
-import urllib.request
+# Jonathan Ho
+# CS 4395
+
+'''This program crawls through a starting website to find other links relevant to a specific term.
+It will then make files for each unique URL, clean up the text, and print out the 25 most used words.
+Based off this, I created a list of 10 significant terms used to create a simple dictionary knowledge base'''
+
+import os.path
 import re
-import os
-import pickle
+import urllib.request
+import requests
 from bs4 import BeautifulSoup
 from nltk import word_tokenize
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
-from itertools import islice
-from functools import reduce
 
+# Function to build the knowledge base
+def build_kb(sig_terms):
+    # Create dictionary with empty list
+    built_base = {x: [] for x in sig_terms}
 
+    file_num = 1
+    doc_name = "URL " + str(file_num) + ".txt"
 
-STARTER_URL = "https://www.epicurious.com" 
+    # Append the sentence to its respective significant term
+    while os.path.isfile(doc_name):
+        with open(doc_name, 'r', encoding='utf-8') as f:
+            sent = f.readlines()
+            for line in sent:
+                for word in sig_terms:
+                    if word in line:
+                        built_base[word].append(line)
 
+        file_num += 1
+        doc_name = "URL " + str(file_num) + ".txt"
 
-def crawler():
-    reqs = requests.get(STARTER_URL)
-    data = reqs.text
-    soup = BeautifulSoup(data, features="lxml")
-    with open('urls.txt', 'w') as file:
+    return built_base
+
+# Private function to get the significant terms
+def _get_sig_terms(file):
+    sw_list = stopwords.words('english')
+    tf_dict = {}
+    tok = word_tokenize(file)
+    tok = [x.lower() for x in tok if x.isalpha() and x not in sw_list]
+
+    for t in tok:
+        if t in tf_dict:
+            tf_dict[t] += 1
+        else:
+            tf_dict[t] = 1
+
+    for token in tf_dict.keys():
+        tf_dict[token] = tf_dict[token] / len(tok)
+
+    return tf_dict
+
+# Find and print out important terms of each file
+def print_file_terms():
+    file_num = 1
+    doc_name = "URL " + str(file_num) + ".txt"
+
+    while os.path.isfile(doc_name):
+        print("")
+        print(doc_name + " important terms:")
+        with open(doc_name, 'r', encoding='utf-8') as f:
+            term_freq = _get_sig_terms(f.read())
+            term_freq = sorted(term_freq.items(), key=lambda x: x[1], reverse=True)
+            print(term_freq[:25])
+
+        file_num += 1
+        doc_name = "URL " + str(file_num) + ".txt"
+
+# Writes sentences of each url into its respective file
+def scrape_url():
+    num = 1
+
+    # Replaces any escape sequences
+    def replace_esc_seq(url_text):
+        # Takes out any escape sequences in the beginning or end of string
+
+        for line in url_text:
+            
+            print("This is the line:", line)
+            line = re.sub(r'[/+=&.?!,:;()©\“”’\'\-\d]', '', line)
+
+        with open('log.txt', 'a', encoding='utf-8') as f:
+            for url in url_text:
+                f.write(url + '\n')
+       # new_list = [x.strip() for x in url_text]
+
+        # Remove empty strings from list
+        #while '' in new_list:
+        #    new_list.remove('')
+
+        # Replace newlines and tabs
+        #new_list = [x.replace('\n', '') for x in new_list]
+        #new_list = [x.replace('\t', '') for x in new_list]
+
+        #return new_list
+        return url_text
+
+    file_name = "URL " + str(num) + ".txt"
+
+    # Goes through each file that contains the name "URL"
+    while os.path.isfile(file_name):
+        with open(file_name, 'r', encoding='utf-8') as fl:
+            raw_lines = fl.readlines()
+            new_lines = replace_esc_seq(raw_lines)  # apply function
+            text = ' '.join(new_lines)
+            sentence = sent_tokenize(text)
+
+        with open(file_name, 'w', encoding='utf-8') as new_file:
+            for sent in sentence:
+                # New line for each sentence in file
+                new_file.write(sent + '\n')
+
+        num += 1
+        file_name = "URL " + str(num) + ".txt"
+
+# Function to get the text within links
+def get_url_text():
+    num = 1
+
+    with open('unique_urls.txt', 'r') as uu:
+        urls = uu.readlines()
+
+        for link in urls:
+            url_link = link
+
+            def visible(element):
+                if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+                    return False
+                elif re.match('<!--.*-->', str(element.encode('utf-8'))):
+                    return False
+                return True
+
+            # Tries to open url, otherwise gives up and goes to next url
+            try:
+                html = urllib.request.urlopen(url_link, timeout=15)
+                soup = BeautifulSoup(html, features="html.parser")
+                data = soup.findAll(string=True)
+                result = filter(visible, data)
+                temp_list = list(result)
+                temp_str = ' '.join(temp_list)
+                file_name = 'URL ' + str(num) + '.txt'
+                print("File " + str(num) + " done.")
+                num += 1
+
+                with open(file_name, 'w', encoding="utf-8") as f:
+                    f.write(temp_str)
+
+            except:
+                print("ERROR: reading timed out leaving unfinished text")
+                continue
+
+# Function to remove any duplicate links
+def remove_dupes():
+    url_list = []
+
+    with open('urls.txt', 'r') as f:
+        urls = f.read().splitlines()
+    for u in urls:
+        url_list.append(u)
+
+    # List comprehension to remove any duplicate links
+    new_list = []
+    [new_list.append(x) for x in url_list if x not in new_list]
+
+    # Create new txt file with no dupes
+    with open('unique_urls.txt', 'w') as nf:
+        for link in new_list:
+            nf.write(str(link) + '\n')
+
+# Function to webcrawl given a link
+def web_crawler(link):
+    r = requests.get(link)
+
+    data = r.text
+    soup = BeautifulSoup(data, features="html.parser")
+
+    # Writes urls to a urls.txt file
+    with open('urls.txt', 'w') as f:
         for link in soup.find_all('a'):
             link_str = str(link.get('href'))
-            if 'recipe' in link_str or 'ingredient' in link_str:
-                file.write(link_str + '\n')
+            if 'Recipe' in link_str or 'recipe' in link_str:
+                if link_str.startswith('/url?q='):
+                    link_str = link_str[7:]
+                if '&' in link_str:
+                    i = link_str.find('&')
+                    link_str = link_str[:i]
+                if link_str.startswith('http') and 'google' not in link_str:
+                    f.write(link_str + '\n')
 
-def scrapper():
-    """Scraps the STARTER_URL
-    
-    Input: none
-    Returns: none
-    
-    Side effect: creates urls.txt file and writes scrapped urls to that file"""
-    with open('urls.txt', 'r') as file:
-        for my_url in file:
-            if 'http' not in my_url:
-                my_url = STARTER_URL + my_url
-            html = urllib.request.urlopen(my_url)
-            soup = BeautifulSoup(html, features='lxml')
-            data = soup.findAll(text=True)
-            result = filter(visible, data)
-            temp_list = list(result)      # list from filter
-            temp_str = ' '.join(temp_list)
-            write_text_to_file(my_url.replace('/', '').strip() + '.txt', temp_str, "w")
+if __name__ == '__main__':
+    link_to_crawl = "https://www.google.com/search?q=recipe&sxsrf=APwXEdcDmMo3SWbYW8v0aiOiORGWqkpsoQ%3A1681266908574&ei=3Bg2ZLSwIpK5qtsPjKmG2Ao&oq=recipie&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAxgCMgQIIxAnMgoIABCABBAUEIcCMgcIABCABBAKMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoKCAAQRxDWBBCwAzoKCAAQigUQsAMQQzoICAAQigUQkQI6EAgAEIAEEBQQhwIQsQMQgwE6CAgAEIAEELEDSgQIQRgAUMUDWKQFYIcfaAFwAXgAgAFTiAGZAZIBATKYAQCgAQHIAQrAAQE&sclient=gws-wiz-serp"
 
+    # Functions to web crawl, create files, and print out term frequencies of each file
+    #web_crawler(link_to_crawl)
+    #remove_dupes()
+    #get_url_text()
+    scrape_url()
+    print_file_terms()
 
-def sentence_tokenization(text: str): 
-    text = re.sub(r'[\t\n]',' ', text.lower())     # remove tabs and newlines
-    sentences = sent_tokenize(text)
-    return sentences
+    # 10 important terms from all files
+    sig_terms = ['mario', 'nintendo', 'game', 'original', 'princess', 'switch', 'character', 'world', 'luigi', 'mushroom']
 
+    # Creating the knowledge base
+    knowledge_base = build_kb(sig_terms)
 
-def parse_text_file(file_name: str) -> str:
-    """Parse a text file into a string
-    Input: file name
-    Returns: string
-    """
-    try:
-        with open(file_name) as file:
-            contents = file.read()
-    except OSError as err:
-        print("OS error:" , err)
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
-    finally:
-        return contents
+    # Printing keys of knowledge base
+    print("")
+    print(knowledge_base.keys())
 
-
-def extract_important_terms(contents: str):
-    """Extract most frequent words from contents
-    
-    Input: string
-    Returns: dict of item: count
-    """
-    contents = re.sub(r'[/+=&.?!,:;()©\“”’\'\-\n\d]',' ', contents.lower())
-    tokens = word_tokenize(contents)
-    stop_words = set(stopwords.words('english'))
-    tokens = [t for t in tokens if not t in stop_words]
-    count_tokens_dict = {t:tokens.count(t) for t in set(tokens)}
-    return count_tokens_dict 
-
-
-def write_text_to_file(file_name: str, text: str, mode: str):
-    with open(os.path.join(os.getcwd(), file_name), mode) as file:
-        file.write(text)
-        
-
-def visible(element):
-    """Helper function to determin if an element is visible"""
-    if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
-        return False
-    elif re.match('<!--.*-->', str(element.encode('utf-8'))):
-        return False
-    return True
-
-
-def reducer(accumulator, element):
-    """Helper function to combine the list of dicts"""
-    for key, value in element.items():
-        accumulator[key] = accumulator.get(key, 0) + value
-    return accumulator
-
-
-if __name__ == "__main__":
-    crawler()
-    scrapper()
-
-
-    list_important_terms = list()
-    with open('urls.txt', 'r') as file:
-        for my_url in file:
-            if 'http' not in my_url:
-                my_url = STARTER_URL + my_url
-            contents = parse_text_file(my_url.replace('/', '').strip() + '.txt')
-            sentences = sentence_tokenization(contents)
-            for sentence in sentences:
-                write_text_to_file(my_url.replace('/', '').strip() + '.out.txt', sentence + '\n', "a")
-            
-            important_terms = extract_important_terms(contents)
-            list_important_terms.append(dict(important_terms))
-            
-        
-
-    # combine the most frequent terms of each page into a sorted dict
-    total = reduce(reducer, list_important_terms, {})
-    sorted_dict = sorted(total.items(), key=lambda x:x[1], reverse=True)
-    top_words = dict(islice(dict(sorted_dict).items(), 45))
-    print(top_words)
-    
-    knowledge_base = list() 
-    with open('urls.txt', 'r') as file:
-        for my_url in file:
-            if 'http' not in my_url:
-                my_url = STARTER_URL + my_url
-            with open(my_url.replace('/', '').strip() + '.out.txt', 'r') as readfile:
-                for sentence in readfile:
-                    if "cake" in sentence:
-                        knowledge_base.append(sentence)
-                    elif "rice" in sentence:
-                        knowledge_base.append(sentence)
-                    elif "chicken" in sentence:
-                        knowledge_base.append(sentence)
-                    elif "food" in sentence:
-                        knowledge_base.append(sentence)
-                    elif "chocolate" in sentence:
-                        knowledge_base.append(sentence)
-                    elif "cook" in sentence:
-                        knowledge_base.append(sentence)
-                    elif "cookie" in sentence:
-                        knowledge_base.append(sentence)
-                    elif "baking" in sentence:
-                        knowledge_base.append(sentence)
-                    elif "sauce" in sentence:
-                        knowledge_base.append(sentence)
-                    elif "cream" in sentence:
-                        knowledge_base.append(sentence)
-
-    pickle.dump(knowledge_base, open('knowledge_base.p', 'wb'))
+    # Commented out because the dictionary is HUGE
+    #for x, y in knowledge_base.items():
+    #    print(x, y)
